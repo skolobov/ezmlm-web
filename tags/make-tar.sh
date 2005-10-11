9e6041800234b2f141389029dfc8b652fe21f49c
lars <lars@b18efcad-8be8-0310-9331-9e7012f9177c>
2005-10-11 21:27:44 +0000

    make-tar.sh cleaned up

--- tags/make-tar.sh
+++ tags/make-tar.sh
@@ -5,4 +5,9 @@ set -eu
 [ $# -ne 1 ] && echo "Syntax: `basename $0` VERSION" && echo && exit 1
 [ ! -d "ezmlm-web-${1}" ] && echo "the directory 'ezmlm-web-${1}' does not exist!" && exit 2
 
-tar czf ezmlm-web-${1}.tar.gz --owner=0 --group=0 --exclude=.svn ezmlm-web-${1}
+TMP_DIR=/tmp/ezmlm-web-${1}
+[ -e "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+
+svn export "ezmlm-web-${1}" "$TMP_DIR"
+tar czf ezmlm-web-${1}.tar.gz -C "$(dirname $TMP_DIR)" --owner=0 --group=0 "$(basename $TMP_DIR)"
+rm -rf "$TMP_DIR"
