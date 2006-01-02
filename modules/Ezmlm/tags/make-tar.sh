04f179048691d2229531916de07113ab7e83b40e
lars <lars@b18efcad-8be8-0310-9331-9e7012f9177c>
2006-01-02 23:00:48 +0000

    added tags for 0.06 and 0.07

---
+++ modules/Ezmlm/tags/make-tar.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+PREFIX=Ezmlm
+
+[ $# -ne 1 ] && echo "Syntax: `basename $0` VERSION" && echo && exit 1
+[ ! -d "${PREFIX}-${1}" ] && echo "the directory '${PREFIX}-${1}' does not exist!" && exit 2
+
+TMP_DIR=/tmp/${PREFIX}-${1}
+[ -e "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+
+svn export "${PREFIX}-${1}" "$TMP_DIR"
+tar czf "packages/${PREFIX}-${1}.tar.gz" -C "$(dirname $TMP_DIR)" --owner=0 --group=0 "$(basename $TMP_DIR)"
+rm -rf "$TMP_DIR"
