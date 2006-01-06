b360c9e2d7499e95801fe79b3ef3ac67b5399691
lars <lars@b18efcad-8be8-0310-9331-9e7012f9177c>
2006-01-06 03:56:50 +0000

    upload 3.0.1

--- tags/make-tar.sh
+++ tags/make-tar.sh
@@ -2,12 +2,14 @@
 
 set -eu
 
-[ $# -ne 1 ] && echo "Syntax: `basename $0` VERSION" && echo && exit 1
-[ ! -d "ezmlm-web-${1}" ] && echo "the directory 'ezmlm-web-${1}' does not exist!" && exit 2
+PREFIX=ezmlm-web
 
-TMP_DIR=/tmp/ezmlm-web-${1}
+[ $# -ne 1 ] && echo "Syntax: `basename $0` VERSION" && echo && exit 1
+[ ! -d "${PREFIX}-${1}" ] && echo "the directory '${PREFIX}-${1}' does not exist!" && exit 2
+
+TMP_DIR=/tmp/${PREFIX}-${1}
 [ -e "$TMP_DIR" ] && rm -rf "$TMP_DIR"
 
-svn export "ezmlm-web-${1}" "$TMP_DIR"
-tar czf packages/ezmlm-web-${1}.tar.gz -C "$(dirname $TMP_DIR)" --owner=0 --group=0 "$(basename $TMP_DIR)"
+svn export "${PREFIX}-${1}" "$TMP_DIR"
+tar czf "packages/${PREFIX}-${1}.tar.gz" -C "$(dirname $TMP_DIR)" --owner=0 --group=0 "$(basename $TMP_DIR)"
 rm -rf "$TMP_DIR"
