25bede179923c2e88f502d91107da020bdb603cb
lars <lars@b18efcad-8be8-0310-9331-9e7012f9177c>
2006-02-15 16:37:26 +0000

    new release 3.1.2

---
+++ tags/ezmlm-web-3.1.2/template/footer.cs
@@ -0,0 +1,8 @@
+
+	<div id="footer">
+		<a href="https://systemausfall.org/toolforge/ezmlm-web">ezmlm-web</a> (v3.1.2) - <?cs var:html_escape(Lang.Misc.FooterText) ?> <a href="http://www.ezmlm.org/" target="_blank">ezmlm</a>
+		<br />
+	</div>
+
+</body>
+</html>
